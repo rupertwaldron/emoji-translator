c99fcfe8ac17c42f7d61043b59ca8b6bfb78a518
rupertwaldron <rupert.waldron@yahoo.co.uk>
2023-07-28 18:12:48 +0100

    Controller and integrationTests working

--- src/main/java/com/ruppyrup/controller/EmojiController.java
+++ src/main/java/com/ruppyrup/controller/EmojiController.java
@@ -2,14 +2,20 @@ package com.ruppyrup.controller;
 
 import com.ruppyrup.model.Message;
 import com.ruppyrup.translator.Translator;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
 @RestController
 @RequestMapping("emoji")
 public class EmojiController {
+
+    private static final Logger logger = LogManager.getLogger(EmojiController.class);
 
     private final Translator emojiTranslator;
 
@@ -25,12 +31,14 @@ public class EmojiController {
     @GetMapping("/encode/{text}")
     public String encode(@PathVariable String text) {
         Message message = new Message(text);
+        logger.info("Encoding message: " + text);
         return message.encode(emojiTranslator);
     }
 
     @GetMapping("/decode/{emojis}")
     public String decode(@PathVariable String emojis) {
         Message message = new Message(emojis);
+        logger.info("Decoding message: " + emojis);
         return message.decode(emojiTranslator);
     }
 
